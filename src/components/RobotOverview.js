2fd255355156fc0777551995641adf9dd6b5e917
Drew Williams <williams.r.drew@gmail.com>
2021-09-02 15:13:55 -0400

    finished up robot cards

--- src/components/RobotOverview.js
+++ src/components/RobotOverview.js
@@ -3,37 +3,46 @@ import PropTypes from 'prop-types'
 import PreviewCompatibleImage from '../components/PreviewCompatibleImage'
 
 const RobotOverview = ({ name, image, wins, losses, awards, events }) => (
-  <div className="container">
-    <h4>Robot Name: {name}</h4>
-    <PreviewCompatibleImage
-      imageInfo={{
-        image: image,
-        alt: `picture of robot`,
-      }}
-    />
-    <span>
-      Record: {wins}W : {losses}L
-    </span>
-    <h5>Events:</h5>
-    {events.map((award, index) => (
-      <div key={index}>
-        <p>{award}</p>
+  <div className="robot-info-container">
+    <div className="columns">
+      <div className="column">
+        <h4 style={{ textAlign: "center" }}>Robot Name: {name}</h4>
+        <PreviewCompatibleImage
+          imageInfo={{
+            image: image,
+            alt: `picture of robot`,
+            style: { margin: "0px auto", width: "75%", borderRadius: "10px" }
+          }}
+        />
       </div>
-    ))}
-    <h5>Awards:</h5>
-    {awards.map((award, index) => (
-      <div key={index}>
-        <p>{award}</p>
+      <div className="column">
+        <h5 style={{ textAlign: "center", marginBottom: "0" }}>
+          Record: {wins} Wins - {losses} Losses
+        </h5>
+        <hr className="solid" color="#E6E8E6" style={{ width: "50%" }}></hr>
+        <h5 style={{ textAlign: "center", marginBottom: "0" }}>Events:</h5>
+        {events.map((award, index) => (
+          <div key={index}>
+            <p style={{ textAlign: "center" }}>{award}</p>
+          </div>
+        ))}
+        <hr className="solid" color="#E6E8E6" style={{ width: "50%" }}></hr>
+        <h5 style={{ textAlign: "center", marginBottom: "0" }}>Awards:</h5>
+        {awards.map((award, index) => (
+          <div key={index}>
+            <p style={{ textAlign: "center" }}>{award}</p>
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
+    </div>
+  </div >
 )
 
 RobotOverview.propTypes = {
   name: PropTypes.string,
-  image: PropTypes.string,
-  wins: PropTypes.string,
-  losses: PropTypes.string,
+  image: PropTypes.object,
+  wins: PropTypes.number,
+  losses: PropTypes.number,
   awards: PropTypes.array,
   events: PropTypes.array
 }
--- src/templates/gamehistory.js
+++ src/templates/gamehistory.js
@@ -7,6 +7,8 @@ import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
 import RobotOverview from '../components/RobotOverview'
 
+const CORSStr = "?origin=https://team2053.org";
+
 export const GameHistoryTemplate = ({
   content,
   contentComponent,
@@ -15,7 +17,13 @@ export const GameHistoryTemplate = ({
   title,
   helmet,
   game_video_url,
-  robot_video_url
+  robot_video_url,
+  robot_name,
+  image,
+  wins,
+  losses,
+  awards,
+  events,
 }) => {
   const PostContent = contentComponent || Content
 
@@ -30,17 +38,17 @@ export const GameHistoryTemplate = ({
             </h1>
             <p>{description}</p>
             <PostContent content={content} />
-            <RobotOverview name="TESTNAME" image="img/logo.svg" wins="54" losses="2" awards={["BIG AWARD", "HUGE AWARD"]} events={["PAVA", "MEME"]} />
+            <RobotOverview name={robot_name} image={image} wins={wins} losses={losses} awards={awards} events={events} />
             {game_video_url ? (
               <div style={{ marginTop: `4rem` }}>
                 <h3>Game Reveal Video</h3>
-                <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + game_video_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + game_video_url + CORSStr} title="YouTube video player 1"></iframe>
               </div>
             ) : null}
             {robot_video_url ? (
               <div style={{ marginTop: `4rem` }}>
                 <h3>Robot Reveal Video</h3>
-                <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + robot_video_url} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe width="560" height="315" src={"https://www.youtube.com/embed/" + robot_video_url + CORSStr} title="YouTube video player 2"></iframe>
               </div>
             ) : null}
             {tags && tags.length ? (
@@ -69,7 +77,13 @@ GameHistoryTemplate.propTypes = {
   title: PropTypes.string,
   helmet: PropTypes.object,
   game_video_url: PropTypes.string,
-  robot_video_url: PropTypes.string
+  robot_video_url: PropTypes.string,
+  robot_name: PropTypes.string,
+  image: PropTypes.object,
+  wins: PropTypes.number,
+  losses: PropTypes.number,
+  awards: PropTypes.array,
+  events: PropTypes.array,
 }
 
 const GameHistoryEntry = ({ data }) => {
@@ -94,6 +108,12 @@ const GameHistoryEntry = ({ data }) => {
         title={post.frontmatter.title}
         game_video_url={post.frontmatter.game_video_url}
         robot_video_url={post.frontmatter.robot_video_url}
+        robot_name={post.frontmatter.robot_name}
+        image={post.frontmatter.image}
+        wins={post.frontmatter.wins}
+        losses={post.frontmatter.losses}
+        awards={post.frontmatter.awards}
+        events={post.frontmatter.events}
       />
     </Layout>
   )
@@ -119,6 +139,18 @@ export const pageQuery = graphql`
         tags
         game_video_url
         robot_video_url
+        robot_name
+        image {
+          childImageSharp {
+            fluid(maxWidth: 700, quality: 100) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+        wins
+        losses
+        awards
+        events
       }
     }
   }
